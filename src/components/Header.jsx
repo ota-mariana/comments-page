758fcb34c9d66630cde1703a0ff2dc5a61f3434d
Mariana <nanomarii10@gmail.com>
2023-03-09 07:26:55 -0300

    feat: add logo image and header style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 import styles from './Header.module.css'
+import igniteLogo from '../assets/ignite-logo.svg'
 
 export function Header() {
   return (
-    <h1 className={ styles.header }>LOGO</h1>
+    <header className={ styles.header }>
+      <img src={ igniteLogo } alt="Logotipo do ignite" />
+      <h1>Ignite Feed</h1>
+    </header>
   )
 }
